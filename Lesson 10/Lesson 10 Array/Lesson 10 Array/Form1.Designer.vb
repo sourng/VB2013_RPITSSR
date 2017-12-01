2b95a33af1807cc7f1660098988d6e5c9abcd45c
sourng <sourng2009@gmail.com>
2017-12-01 10:48:15 +0700

    Update Lesson 10

---
+++ Lesson 10/Lesson 10 Array/Lesson 10 Array/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnTestArray = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnTwoDimenssion = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnTestArray
+        '
+        Me.btnTestArray.Location = New System.Drawing.Point(393, 223)
+        Me.btnTestArray.Name = "btnTestArray"
+        Me.btnTestArray.Size = New System.Drawing.Size(135, 42)
+        Me.btnTestArray.TabIndex = 0
+        Me.btnTestArray.Text = "Testing Array"
+        Me.btnTestArray.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(359, 82)
+        Me.ListBox1.TabIndex = 1
+        '
+        'btnTwoDimenssion
+        '
+        Me.btnTwoDimenssion.Font = New System.Drawing.Font("Khmer OS Battambang", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTwoDimenssion.Location = New System.Drawing.Point(393, 164)
+        Me.btnTwoDimenssion.Name = "btnTwoDimenssion"
+        Me.btnTwoDimenssion.Size = New System.Drawing.Size(135, 40)
+        Me.btnTwoDimenssion.TabIndex = 2
+        Me.btnTwoDimenssion.Text = "Array ២ វិមាត្រ"
+        Me.btnTwoDimenssion.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.Location = New System.Drawing.Point(21, 120)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(349, 173)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(393, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(151, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(401, 59)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(143, 35)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(570, 328)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.btnTwoDimenssion)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btnTestArray)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnTestArray As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents btnTwoDimenssion As System.Windows.Forms.Button
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Lesson 10/Lesson 10 Array/Lesson 10 Array/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Dim CustName(3, 4) As String
+
+    Private Sub btnTestArray_Click(sender As Object, e As EventArgs) Handles btnTestArray.Click
+        Dim CusName(6) As String  'ការ​បង្កើត​ Array
+
+        CusName(0) = "Thida"
+        CusName(1) = "Bopha"
+        CusName(2) = "Nara"
+
+
+        Dim i As Integer
+        For i = 0 To CusName.Length - 1
+            CusName(i) = InputBox("Enter Name :")
+        Next
+
+        For i = 0 To CusName.Length - 1
+            ListBox1.Items.Add(CusName(i))
+        Next
+
+        'MsgBox("ប្រវែង​របស់​ Array មាន​ :" & CusName.Length)
+
+
+    End Sub
+
+    Private Sub btnTwoDimenssion_Click(sender As Object, e As EventArgs) Handles btnTwoDimenssion.Click
+
+        CustName(0, 0) = "003"
+        CustName(0, 1) = "Kaka"
+        CustName(0, 2) = "F"
+        CustName(0, 3) = "20"
+
+        CustName(1, 0) = "001"
+        CustName(1, 1) = "Thida"
+        CustName(1, 2) = "M"
+        CustName(1, 3) = "28"
+
+        CustName(2, 0) = "002"
+        CustName(2, 1) = "Bopha"
+        CustName(2, 2) = "F"
+        CustName(2, 3) = "19"
+
+        Dim i As Integer
+        For i = 0 To 2
+            'For j = 0 To 3
+            'ListBox1.Items.Add(CusName(i, 0) & "  " & CusName(i, 1) & "  " & CusName(i, 2) & " " & CusName(i, 3))
+
+            ListView1.Items.Add(CustName(i, 0))
+            ListView1.Items(i).SubItems.Add(CustName(i, 1))
+            ListView1.Items(i).SubItems.Add(CustName(i, 2))
+            ListView1.Items(i).SubItems.Add(CustName(i, 3))
+
+            'Next
+        Next
+    End Sub
+
+ 
+End Class
