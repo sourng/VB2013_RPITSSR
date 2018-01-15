ee9e9324641a57324ed5247b628d2353a129dabc
SENG Sourng <sourng2009@gmail.com>
2018-01-15 20:13:26 +0700

    update

---
+++ Zoom Image/Zoom Image/Zoom Image/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ResetToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ZoomInToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ZoomOutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ResetToolStripMenuItem1, Me.ZoomInToolStripMenuItem, Me.ZoomOutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(719, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ResetToolStripMenuItem1
+        '
+        Me.ResetToolStripMenuItem1.Name = "ResetToolStripMenuItem1"
+        Me.ResetToolStripMenuItem1.Size = New System.Drawing.Size(148, 20)
+        Me.ResetToolStripMenuItem1.Text = "PanToolStripMenuItem1"
+        '
+        'ZoomInToolStripMenuItem
+        '
+        Me.ZoomInToolStripMenuItem.Name = "ZoomInToolStripMenuItem"
+        Me.ZoomInToolStripMenuItem.Size = New System.Drawing.Size(64, 20)
+        Me.ZoomInToolStripMenuItem.Text = "Zoom In"
+        '
+        'ZoomOutToolStripMenuItem
+        '
+        Me.ZoomOutToolStripMenuItem.Name = "ZoomOutToolStripMenuItem"
+        Me.ZoomOutToolStripMenuItem.Size = New System.Drawing.Size(74, 20)
+        Me.ZoomOutToolStripMenuItem.Text = "Zoom Out"
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "1.png")
+        Me.ImageList1.Images.SetKeyName(1, "7.png")
+        Me.ImageList1.Images.SetKeyName(2, "5.png")
+        Me.ImageList1.Images.SetKeyName(3, "3.png")
+        Me.ImageList1.Images.SetKeyName(4, "2.png")
+        Me.ImageList1.Images.SetKeyName(5, "6.PNG")
+        Me.ImageList1.Images.SetKeyName(6, "4.png")
+        '
+        'ListView1
+        '
+        Me.ListView1.LargeImageList = Me.ImageList1
+        Me.ListView1.Location = New System.Drawing.Point(87, 42)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(433, 218)
+        Me.ListView1.SmallImageList = Me.ImageList1
+        Me.ListView1.TabIndex = 1
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(719, 352)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ResetToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ZoomInToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ZoomOutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+
+End Class
---
+++ Zoom Image/Zoom Image/Zoom Image/Form1.vb
@@ -0,0 +1,27 @@
+﻿
+Option Explicit On
+Imports System.IO ' for filestream functionality
+
+Public Class Form1
+    Dim m_PanStartPoint As New Point 'for Pan Function
+    Dim DrawRectangle As Boolean = False 'for draw rectangle function
+    Dim rect As Rectangle 'for draw rectangle function
+    Dim DrawRectangle_Mouse_X_Start As Integer 'for draw rectangle function
+    Dim DrawRectangle_Mouse_Y_Start As Integer 'for draw rectangle function
+    Dim DrawRectangle_Mouse_X_End As Integer 'for draw rectangle function
+    Dim DrawRectangle_Mouse_Y_End As Integer 'for draw rectangle function
+    Dim currentfilename As String = Nothing 'copy of current image filename
+    Dim original As Image = Nothing 'original image used for zoom feature
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As Integer
+        For i = 1 To 7
+            Me.ListView1.Items.Add("Hello" & i, i & ".png")
+        Next
+    End Sub
+
+    Private Sub ResetToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ResetToolStripMenuItem1.Click
+
+    End Sub
+End Class
